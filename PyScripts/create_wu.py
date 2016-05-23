72c1ba293518dcff0fbc30a16076cf1689d8a3c9
DumMai <dummai@users.noreply.github.com>
2016-05-24 00:14:22 +0700

    Create create_wu.py

---
+++ PyScripts/create_wu.py
@@ -0,0 +1,7 @@
+file = open("create_work.sh","w") #create a bash file "create_work.sh" for running "create_work" commands in batch.
+file.write("#!/bin/bash\n")
+for i in range(some_number): #some_number = the last running number of WU name (e.g. work_name_1, work_name_2,...) and files of data chunks (e.g. file_name_1.fa, file_name_2.fa,...) 
+    create = """./bin/create_work --appname blastn --wu_name work_name_%i 
+    --wu_template templates/blastn_wu --result_template templates/blastn_re file_name_%i.fa db.nhr db.nin db.nsq blastn.exe\n""" %((i+1),(i+1))
+    file.write(create)
+file.close()
